dd16f3ab465d5c0ff7c19f03f001580b389db5f3
igphoenix6639 <igphoenix6639@gmail.com>
2020-04-02 23:21:01 +0200

    prije controllera

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const http = require('http')
+const Utils = require('utils.js')
 
 const server = http.createServer((req,res) =>{
-    res.writeHead(200, {'Content-type': 'text/plain'})
-    res.write('Ovo je basic crap')
-    res.end()
+    let url = Utils.sanitize(req.url)
+
+    switch(url){
+        case "":
+            res.end("Main page")
+            break;
+        default:
+            res.end("Default crap")
+            break;
+
+
+    }
 }).listen(3000, '127.0.0.1', function(error){
     if(error){
         console.log('Some shit went wrong')
